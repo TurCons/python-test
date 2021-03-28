a8d61708226a3557f7f69d1d4b8714541561a80d
Constantine <const89@yandex.ru>
2021-03-28 17:10:26 +0500

    task 2-4 + input description

--- python-test2-4.py
+++ python-test2-4.py
@@ -1,26 +1,28 @@
-# задание №2-3
+# задание №2-4
+# Задание Python 2_4: Исправить предыдущее задание (2_3) для работы со случайными числами.
+
 # Задание Python 2_3: Запрашиваются 10 чисел (целые значения от 0 до 1000).     
 # Опишите алгоритм, позволяющий найти и вывести минимальное значение среди введенных чисел, которые имеют чётное значение и не делятся на три.
+
+from random import randint
 
 # левая граница диапазона
 leftBound = 0
 # правая граница диапазона
 rightBound = 1000
 
-print('Введите 10 чисел в диапазоне [' + str(leftBound) + ', ' + str(rightBound) + ']')
-
 numbersCount = 0
 minNumber = rightBound + 1
 
 while numbersCount < 10:
-    number = int(input())
-    
-    if (number >= leftBound and number <= rightBound):
-        numbersCount += 1
-        if (number < minNumber and number % 2 == 0 and number % 3 != 0):
-            minNumber = number
-    else:
-        print('Введенное число не попадает в диапазон [' + str(leftBound) + ', ' + str(rightBound) + ']')
+    # генерируем случайное целое число в диапазоне [leftBound, rightBound]
+    number = randint(leftBound, rightBound)
+
+    print(number)
+    numbersCount += 1
+
+    if (number < minNumber and number % 2 == 0 and number % 3 != 0):
+        minNumber = number
 
 
 if minNumber < rightBound + 1:
