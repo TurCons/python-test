bebc1afc69f062653e5dd46817a45f4c33d55d6c
Constantine <const89@yandex.ru>
2021-03-28 14:25:03 +0500

    task 2-2

---
+++ python-test2-2.py
@@ -0,0 +1,16 @@
+# задание №2-2
+
+# количество чисел для вывода
+numbersCount = int(input())
+
+n1 = 1  # первое число
+n2 = 1  # второе число
+print(n1)
+print(n2)
+nn = 0  # n-тое число
+
+for number in range(numbersCount-2):
+    nn = n1 + n2
+    n1 = n2
+    n2 = nn
+    print(nn)
